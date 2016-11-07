baf65509a2d3cb27570e006324dc8aa15f4405b2
yohann.meyer <yohann.meyer@gmail.com>
2016-11-07 13:46:38 +0100

    better version 2

--- INF1/labo4/Labo_Meyer_rouvinez.cpp
+++ INF1/labo4/Labo_Meyer_rouvinez.cpp
@@ -70,33 +70,35 @@ int main() {
         int val, num=2, denum=1;
         int terme =1;
         double valActuel, valPrecedente=2;
-        do{
+        while(true){
             sum *= (double)num / denum;
 
             ecart = abs(sum- valPrecedente);
+            if(ecart <= pow(10,-precision) && ecart !=0)
+                break;
           //  sum *= 4. * pow(num, 2) / (4 * pow(num, 2) - 1);
 
             cout << setw(SET_W_FIRST) << terme << setw(SET_W) << num << "/" << setw(SET_W_FIRST) << denum << setw(SET_W) << setprecision(1+precision) << sum  << setprecision(5) << setw(SET_W) << ecart << endl;
-            if (num > denum) {
+            if (num > denum){
                 denum = denum + 2;
             } else {
                 num = num + 2;
             }
             valPrecedente = sum;
-            terme++;
-        }while(ecart > pow(10,-precision) || ecart ==0);
+            ++terme;
+        }
+            --terme;
+        //TODO add var result
+        cout << "Approximation de de Pi/2: " << setprecision(1+precision) << sum << endl;
+
+
+
+
+
+
 
         //TODO add var result
-        cout << "Approximation de de Pi/2: " << endl;
-
-
-
-
-
-
-
-        //TODO add var result
-        cout << "Nombres de termes: " << endl;
+        cout << "Nombres de termes: " << terme << endl;
 
 
         cout << "Voulez-vous continuer [o/n]:";
