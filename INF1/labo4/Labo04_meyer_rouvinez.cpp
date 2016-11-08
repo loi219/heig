e6fc23a5b7f75fc20cdc969b54763cb85f3bdee8
yohann.meyer <yohann.meyer@gmail.com>
2016-11-08 19:24:30 +0100

    Version final avec commentaires

--- INF1/labo4/Labo04_meyer_rouvinez.cpp
+++ INF1/labo4/Labo04_meyer_rouvinez.cpp
@@ -1,14 +1,16 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : Labo_04_
+ Laboratoire : Labo_04
  Fichier     : Labo_04_meyer_rouvinez.cpp
  Auteur(s)   : Yohann Meyer Sven Rouvinez
  Date        : 06.11.2016
 
  But         : Permet d'approximer la valeur de pi suite à une précision donnée
-               par l'utilisateur
+               par l'utilisateur.
 
- Remarque(s) : Utilisation de la suite de Wallis pour approximer pi
+ Remarque(s) : Utilisation de la suite de Wallis pour approximer pi.
+               Gestion complète des erreurs de saisie
+               
 
  Compilateur : g++ (GCC) 6.2.1 20160916 (Red Hat 6.2.1-2)
  -----------------------------------------------------------------------------------
@@ -16,41 +18,43 @@
 
 #include <iostream>
 #include <cstdlib>
-#include <string>
 #include <climits>
 #include <iomanip>
 #include <cmath>
-
-#define FLUSH_BUFFER while( cin.get() != '\n' )
 
 using namespace std;
 
 int main() {
 
+
     const int   MIN_COUT_PRECISION  = 1;
     const int   MAX_COUT_PRECISION  = 5;
     const int   SET_W               = 10;
-    const int   SET_W_FIRST         = 3;
+    const int   SET_W_NUM         = 3;
+//nombre de chiffres significatifs de l'écart par défaut
     const int   DEFAULT_PRECISION   = 5;
 
-    const       string ERREUR = "Erreur dans la precision";
+// On définit ici les valeurs de depart de la série
+    const int   NUM_INITIAL         = 2;
+    const int   DENUM_INITIAL       = 1;
 
     char        choixContinuer;
-    double      sum,
-                valPrecedente       = 2.,
-                ecart;
+    double      product,
+                previousProduct,
+                ecart,
+                precisionCalc;
     int         num,
                 denum,
                 terme,
                 precision;
     bool        saisieCtrl;
 
-    cout.fixed;
-
     do {
+
+//Boucle de saisie
         do {
 
-            cout << "quelle precision [ 10^-"  << MIN_COUT_PRECISION << " 10^-" << MAX_COUT_PRECISION << "]: 10^-";
+            cout << "Quelle precision [ 10^-"  << MIN_COUT_PRECISION << " 10^-" << MAX_COUT_PRECISION << "]: 10^-";
             saisieCtrl = bool(cin >> precision);
 
 
@@ -61,45 +65,66 @@ int main() {
 
 
         } while (precision < MIN_COUT_PRECISION || precision > MAX_COUT_PRECISION || !saisieCtrl);
+        cin.ignore(INT_MAX, '\n');
 
 
-        cout << setw(SET_W_FIRST) << "No" << setw(SET_W) << "Terme" << setw(SET_W) << "Pi/2" << setw(SET_W) << "Ecart"
+        cout << setw(SET_W_NUM) << "No" << setw(SET_W) << "Terme" << setw(SET_W) << "Pi/2" << setw(SET_W) << "Ecart"
              << endl;
-
-        num     = 2;
-        denum   = 1;
-        sum     = terme = 1;
+/*
+ * Initalisations:
+ * @previousProduct : prend la valeur NUM_INITIAL pour permettre un écart de 0 lors de la première itération
+ * @num             : prend la valeur NUM_INIIAL
+ * @denum           : prend la valeur DENUM_INITIAL
+ * @product         : prend 1. (double), car 1 est l'élément neutre de la multiplication
+ * @terme           : prend 1 car l'incrémentation du nombre de terme se fait après l'impression
+ * @precisionCalc   : prend la valeur du calcul de 10^precision pour éviter de le faire dans la boucle
+ */
+        previousProduct   = NUM_INITIAL;
+        num             = NUM_INITIAL;
+        denum           = DENUM_INITIAL;
+        product             = 1.;
+        terme           = 1;
+        precisionCalc   = pow(10, -precision);
+//Boucle de calcul
         while (true) {
-            sum *= (double) num / denum;
-
-            ecart = abs(sum - valPrecedente);
-            if (ecart <= pow(10, -precision) && ecart)
+            product *= (double) num / denum;
+//abs gare l'écart ne s'intéresse qu'à la magnitude de la différence de n_-1 et n.
+            ecart = abs(product - previousProduct);
+/*
+ * Permet de ne pas calculer un terme de trop.
+ * "&& ecart" permet de ne pas sortir de la boucle quand ecart est égal à 0 (première itération)
+ * [Comme ecart est une valeur définie par une fonction décroissante limitée en 0, on sait qu'on
+ * sortira de la boucle]
+ */
+            if (ecart <= precisionCalc && ecart)
                 break;
-
-            cout << setw(SET_W_FIRST) << terme << setw(SET_W) << num << "/" << setw(SET_W_FIRST) << denum << setw(SET_W)
-                 << setprecision(1 + precision) << sum << setprecision(DEFAULT_PRECISION) << setw(SET_W) << ecart << endl;
+/*
+ * @SET_W_NUM : définit l'espace sur lequel s'affiche le nbr de terme ainsi que le dénominateur.
+ * @SET_W     : définit l'espacement entre chaque colonne. on ajoute precision-1 pour garantir la lisibilité des
+ *              données même quand l'écart devient très petit (10⁻⁵).
+ */
+            cout << setw(SET_W_NUM) << terme << setw(SET_W) << num << "/" << setw(SET_W_NUM) << denum << setw(SET_W)
+                 << setprecision(1 + precision) << product << setprecision(DEFAULT_PRECISION) << setw(SET_W+precision-1) << ecart << endl;
 
             if (num > denum) {
                 denum += 2;
             } else {
                 num += 2;
             }
-
-            valPrecedente = sum;
+/*
+ * le produit calculé et affiché devient le produit précédent de la prochaine itération.
+ */
+            previousProduct = product;
             ++terme;
         }
-        cout << "Approximation de de Pi/2: " << setprecision(1 + precision) << sum << endl;
+        cout << "Approximation de de Pi/2: " << setprecision(1 + precision) << product << endl;
 
         cout << "Nombres de termes: " << terme << endl;
 
 
         cout << "Voulez-vous continuer [o/n]:";
-        saisieCtrl = bool(cin >> choixContinuer);
-        if (!saisieCtrl) {
-            cin.clear();
-            cin.ignore(INT_MAX, '\n');
-        }
-
+        cin  >> choixContinuer;
+        cin.ignore(INT_MAX,'\n');
     } while (choixContinuer != 'n');
 
 
